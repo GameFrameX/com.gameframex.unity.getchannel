efd711913049cdc753ca176f32620201e56de9a9
AlianBlank <alianblank@outlook.com>
2023-07-21 14:30:09 +0800

    [重构] 1. 修改宏定义顺序

--- Runtime/BlankGetChannel.cs
+++ Runtime/BlankGetChannel.cs
@@ -1,16 +1,14 @@
 ﻿#if UNITY_IOS
 using System.Runtime.InteropServices;
 #endif
-using System;
 using System.IO;
-using LuaInterface;
 using UnityEngine;
 
 /// <summary>
 /// 获取渠道名称
 /// Android:
 ///         需要在主启动的Activity 中添加
-/// <meta-data android:name="appchannel" android:value="android_cn_yyyl" />
+/// <meta-data android:name="channel" android:value="android_cn_taptap" />
 ///
 /// iOS :
 ///         需要在Info.plist中添加
@@ -20,9 +18,7 @@ using UnityEngine;
 /// </summary>
 public sealed class BlankGetChannel
 {
-
 #if UNITY_IOS
-	
 	[DllImport("__Internal")]
 	private static extern string getChannelName(string channelKey);
 
@@ -30,20 +26,27 @@ public sealed class BlankGetChannel
     /// <summary>
     /// 获取渠道值
     /// </summary>
-    public static string GetChannelName(string channelKey)
+    public static string GetChannelName(string channelKey = "channel")
     {
         string channelName = "unknown";
 #if UNITY_STANDALONE || UNITY_EDITOR
-        string channel = File.ReadAllText(Application.streamingAssetsPath + "/channel.txt");
-        if (!string.IsNullOrEmpty(channel))
+        string path = Application.streamingAssetsPath + "/channel.txt";
+        if (File.Exists(path))
         {
-            channelName = channel;
+            string channel = File.ReadAllText(Application.streamingAssetsPath + "/channel.txt");
+            if (!string.IsNullOrEmpty(channel))
+            {
+                channelName = channel;
+            }
         }
+
 #elif UNITY_ANDROID
-        AndroidJavaClass androidJavaClass = new AndroidJavaClass("com.alianhome.getchannel.MainActivity");
-        channelName = androidJavaClass.CallStatic<string>("GetChannel", channelKey);
+        using (AndroidJavaClass androidJavaClass = new AndroidJavaClass("com.alianhome.getchannel.MainActivity"))
+        {
+            channelName = androidJavaClass.CallStatic<string>("GetChannel", channelKey);
+        }
 #elif UNITY_IOS
-        channelName= getChannelName(channelKey);
+        channelName = getChannelName(channelKey);
 #endif
         return channelName;
     }
